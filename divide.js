4cbb1dbe683037ac66cdef8aa302e116b952d87b
codewithritik <97462674+codewithritik@users.noreply.github.com>
2022-03-02 20:02:42 +0530

    CALCULATOR

---
+++ divide.js
@@ -0,0 +1,7 @@
+function divide(){
+    let a = 4;
+    let b = 10;
+    return a/b
+}
+
+module.exports = divide
---
+++ subtract.js
@@ -0,0 +1,7 @@
+function subtract(){
+    let a = 4;
+    let b = 10;
+    return a-b
+}
+
+module.exports = subtract
